c0673e6243fb1c28f067b82d4718414e28f54db4
Daróczi Levente <daroczilevente2@gmail.com>
2025-12-19 17:35:31 +0200

    added universal design, footer, navbar improvement, logo

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router';
+import {BrowserRouter, Route, Routes} from 'react-router';
 
 import NavigationBar from './components/NavigationBar.tsx';
 import BlogPostPage from './pages/BlogPostPage.tsx';
@@ -7,21 +7,27 @@ import CartPage from './pages/CartPage.tsx';
 import FavoritesPage from './pages/FavoritesPage.tsx';
 import HomePage from './pages/HomePage.tsx';
 import ProductPage from './pages/ProductPage.tsx';
+import FooterBar from "./components/FooterBar.tsx";
 
 function Root() {
     return (
-        <div>
-            <BrowserRouter>
-                <NavigationBar />
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/product/:productId" element={<ProductPage />} />
-                    <Route path="/cart" element={<CartPage />} />
-                    <Route path="/favorites" element={<FavoritesPage />} />
-                    <Route path="/blog" element={<BlogPostsPage />} />
-                    <Route path="/blog/:blogId" element={<BlogPostPage />} />
-                </Routes>
-            </BrowserRouter>
+        <div className="App">
+            <div className="page-container">
+                <BrowserRouter>
+                    <NavigationBar/>
+                    <main className="content-wrap">
+                        <Routes>
+                            <Route path="/" element={<HomePage/>}/>
+                            <Route path="/product/:productId" element={<ProductPage/>}/>
+                            <Route path="/cart" element={<CartPage/>}/>
+                            <Route path="/favorites" element={<FavoritesPage/>}/>
+                            <Route path="/blog" element={<BlogPostsPage/>}/>
+                            <Route path="/blog/:blogId" element={<BlogPostPage/>}/>
+                        </Routes>
+                    </main>
+                    <FooterBar/>
+                </BrowserRouter>
+            </div>
         </div>
     );
 }
--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,28 +1,32 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
+import {Container, Image, Nav, Navbar} from 'react-bootstrap';
+import {useTranslation} from 'react-i18next';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {FaCartShopping} from "react-icons/fa6";
+import {FaHeart} from "react-icons/fa";
+
 
 import style from './NavigationBar.module.css';
 
 function NavigationBar() {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
 
     return (
-        <Navbar expand="lg" data-bs-theme="dark" className={style.navbarStyle}>
+        <Navbar expand="lg" className={`${style.navbarStyle}`}>
             <Container className={style.gridContainer}>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Brand href="/" className={style.brandCentered}>
+                    <Image src="/logo/logo.png" alt="Logo" className={style.logoStyle}/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className={`ms-auto ${style.navStyle}`}>
                         <Nav.Link href="/blog" className={style.navLinkStyle}>
                             {t('blogposts.title')}
                         </Nav.Link>
                         <Nav.Link href="/favorites" className={style.navLinkStyle}>
-                            {t('favorites.title')}
-                        </Nav.Link>
-                        <Nav.Link href="/home" className={style.navLinkStyle}>
-                            {t('home.title')}
+                            <FaHeart size={20}/>
                         </Nav.Link>
                         <Nav.Link href="/cart" className={style.navLinkStyle}>
-                            {t('cart.title')}
+                            <FaCartShopping size={20}/>
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
