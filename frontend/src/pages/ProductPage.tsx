8b94b83cf10750f3ab57683552d407cf8ee692df
Daróczi Levente <daroczilevente2@gmail.com>
2025-12-21 10:43:17 +0200

    Products page improvement

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,24 +1,24 @@
-import { Button, Image, Table } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
-import { FaHeart } from 'react-icons/fa';
-import { FaCartShopping } from 'react-icons/fa6';
-import { useParams } from 'react-router';
+import {Button, Image, Table} from 'react-bootstrap';
+import {useTranslation} from 'react-i18next';
+import {FaHeart} from 'react-icons/fa';
+import {FaCartShopping} from 'react-icons/fa6';
+import {useParams} from 'react-router';
 
 import StarRating from '../components/StarRating.tsx';
-import { getCoffeeById } from '../services/main.api.ts';
+import {getCoffeeById} from '../services/main.api.ts';
 import style from './ProductPage.module.css';
 
 function ProductPage() {
-    const { productId } = useParams();
+    const {productId} = useParams();
     const product = getCoffeeById(productId ? parseInt(productId) : 0);
 
-    const { t } = useTranslation();
+    const {t} = useTranslation();
 
     return (
         <div className={style.container}>
             <div className={style.mainSection}>
                 <div className={style.leftSection}>
-                    <Image src={product?.imageUrl} alt="product" className={style.image} />
+                    <Image src={product?.imageUrl} alt="product" className={style.image}/>
                 </div>
                 <div className={style.rightSection}>
                     <div>
@@ -27,7 +27,7 @@ function ProductPage() {
                             <h1>{product.name}</h1>
                         </div>
                         <div className={style.ratingContainer}>
-                            <StarRating rating={product.averageRating} size={30} />
+                            <StarRating rating={product.averageRating} size={30}/>
                             <h4>
                                 {product.averageRating} ({product.reviewCount} reviews)
                             </h4>
@@ -37,29 +37,31 @@ function ProductPage() {
                         <h3>{product.price} $</h3>
                         <div className={style.buttonContainer}>
                             <Button variant="dark">
-                                <FaCartShopping /> Add to cart
+                                <FaCartShopping/> Add to cart
                             </Button>
                             <Button variant="dark">
-                                <FaHeart /> Favorites
+                                <FaHeart/> Favorites
                             </Button>
                         </div>
                     </div>
-                    <div className={style.description}>
-                        <h3>Description</h3>
-                        <p>{product.description}</p>
-                    </div>
+
                 </div>
             </div>
             <div className={style.specificationsSection}>
-                <h3>Specifications</h3>
-                <Table striped bordered hover>
-                    <thead>
+                <div className={style.description}>
+                    <h3>Description</h3>
+                    <p>{product.description}</p>
+                </div>
+                <div>
+                    <h3>Specifications</h3>
+                    <Table striped bordered hover>
+                        <thead>
                         <tr>
                             <th>{t('product.specifications')}</th>
                             <th>{t('product.value')}</th>
                         </tr>
-                    </thead>
-                    <tbody>
+                        </thead>
+                        <tbody>
                         <tr>
                             <td>{t('product.roastLevel')}</td>
                             <td>{product.specifications.roastLevel}</td>
@@ -89,8 +91,9 @@ function ProductPage() {
                                 )}
                             </td>
                         </tr>
-                    </tbody>
-                </Table>
+                        </tbody>
+                    </Table>
+                </div>
             </div>
         </div>
     );
