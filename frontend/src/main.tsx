73157ea17771f103bdb0501091eac7d154e335a9
Daróczi Levente <daroczilevente2@gmail.com>
2026-01-18 10:58:59 +0200

    fix comments

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -16,6 +16,5 @@ createRoot(document.getElementById('root')!).render(
                 <Root />
             </CartProvider>
         </FavoritesProvider>
-        ,
     </QueryClientProvider>,
 );
